f2cd37b1af5a7becdc9264cdf1afd70eda79180e
Vanessa <vanessadonadelli2014@hotmail.com>
2025-07-18 22:12:05 -0300

    estudo de função, parametro e argumento, e merge

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function mostrarHelloWorld() {
     const p = document.getElementById("local");
     p.textContent = "Hello world!"
 }
+
+function somar(n1, n2){ // parametro
+  const result = n1 + n2
+  return result
+  console.log('Teste de após return.'); // inacessível por estar depois do return ^_+
+  
+}
+console.log(somar(2, 4)); // argumento
